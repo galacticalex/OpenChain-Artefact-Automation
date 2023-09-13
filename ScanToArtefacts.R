7c98021e1b120ce70d079f55500cbd348833f033
Alexander Murphy <alexander.murphy@orcro.co.uk>
2023-09-13 09:52:29 +0100

    Loading data in script re-write, clarifications and detail on scancode.

--- ScanToArtefacts.R
+++ ScanToArtefacts.R
@@ -48,6 +48,7 @@
 # 
 # The script requires an internet connection to function.
 
+
 ##
 ## Environment variables and setting start ----
 ## 
@@ -65,6 +66,7 @@ args = commandArgs(trailingOnly = T)
 ##
 ## End section ----
 ## 
+
 
 ## 
 ## Error message definitions start ----
@@ -104,28 +106,130 @@ errorMessages = c(errorArgsNumber,
 ## 
 
 
+##
+# File Content Start ----
+##
+
+overviewText = paste0("# Overview\n\nThis software contains a number of open source components. For a summary, see \n", 
+                      "below. For the relevant licence texts, see Appendix A. For the relevant notices \n", 
+                      "and attributions etc., see Appendix B.\n\n", 
+                      "Not all components listed may be incorporated in, or may necessarily have \n", 
+                      "generated derivative works which are incorporated in the firmware, but were \n", 
+                      "used during the build process.\n\n", 
+                      "Where a range of dates is given after a copyright notice, this should be taken \n", 
+                      "to imply that copyright is asserted for every year within that range, inclusive \n", 
+                      "of the years stated.\n\n", 
+                      "## Components and Licences")
+
+appendixAText = "# Appendix A: Licence Texts"
+appendixBText = "# Appendix B: Notices and Attribution"
+
+genericFileContent = c(overviewText, appendixAText, appendixBText)
+
+##
+# File Content End ----
+##   
 
 
+##
+# Function Definitions Start ----
+##
+
+outputFile = function(fileText, fileName) {
+    write(x = fileText, file = fileName)
+}
+
+generateOverviewText = function(softwareName, componentNames, licences) {
+    
+    dat = as.data.frame(cbind(softwareName, 
+                              componentNames, 
+                              licences))
+    
+    dat = split(dat, dat$softwareName)
+    
+    t = function(d) {
+        header = paste0("\n\n### ", d[1, 1], "\n\n")
+        
+        rest = paste0(rep("Component: ", length(d[2])), 
+                      unlist(d[2]), 
+                      rep(" - Licence: ", length(d[3])), 
+                      unlist(d[3]), 
+                      rep("\n", 4), collapse = "")
+        
+        paste0(header, rest)
+    }
+    
+    pre_out = lapply(dat, t)
+    
+    out = paste0(pre_out, collapse = "")
+    
+    paste0(overviewText, out)
+}
+
+generateArtefacts = function() {
+    
+    sca_data <- read.csv(args[1])
+    
+    sca_data <- sca_data[, c(1, 2, 4, 20)]
+    
+    print(names(sca_data))
+    
+    # data2 = read.csv(args[2])
+    # 
+    # outputFile(generateOverviewText(data[[1]], 
+    #                                 data[[2]], 
+    #                                 data[[4]]), 
+    #            artefactFileNames[1])
+    # 
+    # outputFile(generateAppendixA(data[[4]], 
+    #                              data[[8]]), 
+    #            artefactFileNames[2])
+    # 
+    # outputFile(generateAppendixB(data2[[1]], 
+    #                              data2[[19]], 
+    #                              data2[[38]]), 
+    #            artefactFileNames[3])
+}
+
+##
+# Function Definitions End ----
+##
 
 
+##
+# Program Logic Start ----
+##
 
+if (length(args) != 1) {
+    cat(errorArgsNumber)
+} else {
+    generateArtefacts()
+}
 
+#if (length(args) != 2) {
+#    cat(errorMessages[1])
+#} else if (artefactFileNames %in% dir()) {
+#    cat(errorMessages[2])
+#} else if (file.exists(args)) {
+#    generateArtefacts()
+#} else {
+#    cat(errorMessages[3])
+#}
 
-
-
-
+##
+# Program Logic End ----
+##
 
 
 ## 
-## Testing section ----
+## Testing and scratch ----
 ## 
 
-cat(errorArgsNumber)
-print("")
-cat(errorInputMissing)
-print("")
-cat(errorFileExists)
-
+#cat(errorArgsNumber)
+#print("")
+#cat(errorInputMissing)
+#print("")
+#cat(errorFileExists)
 
 ## 
 ## End section ----
